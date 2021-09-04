df9e484f39c8a48dc96e1e6a823e6fe21825ed8e
pra152 <pravatdas211@gmail.com>
2021-09-04 12:14:30 +0530

    Refactor: to throw custom exceptions in invalid user details

--- src/com/blz/juintTest/EmailTest.java
+++ src/com/blz/juintTest/EmailTest.java
@@ -1,40 +1,52 @@
 package com.blz.juintTest;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import com.blz.juintTest.Exception.InvalidInputException;
 import com.blz.userdetails.UserRegistration;
 
 @RunWith(Parameterized.class)
 public class EmailTest {
+	enum Type {
+		VALID, INVALID
+	};
+
+	private Type type;
 	private String userEmail;
 	private boolean expectedResult;
 	UserRegistration userRegistration;
 
-	public EmailTest(String userEmail, boolean expectedResult) {
+	public EmailTest(Type type, String userEmail, boolean expectedResult) {
+		this.type = type;
 		this.userEmail = userEmail;
 		this.expectedResult = expectedResult;
 	}
 
 	@Parameterized.Parameters
 	public static Collection data() {
-		return Arrays.asList(new Object[][] { { "abc@yahoo.com", true }, { "abc-100@yahoo.com", true },
-				{ "abc-100@yahoo.com", true }, { "abc111@abc.com", true }, { "abc-100@abc.net", true },
-				{ "abc.100@abc.com.au", true }, { "abc@1.com", true }, { "abc@gmail.com.com", true },
-				{ "abc+100@gmail.com", true }, { "abc", false }, { "abc@.com.my", false }, { "abc123@gmail.a", false },
-				{ "abc123@.com", false }, { "abc123@.com.com", false }, { ".abc@abc.com", false },
-				{ "abc()*@gmail.com", false }, { "abc@%*.com", false }, { "abc..2002@gmail.com", false },
-				{ "abc.@gmail.com", false }, { "abc@abc@gmail.com", false }, { "abc@gmail.com.1a", false },
-				{ "abc@gmail.com.aa.au", false } });
+
+		return Arrays.asList(new Object[][] { { Type.VALID, "abc@yahoo.com", true },
+				{ Type.VALID, "abc-100@yahoo.com", true }, { Type.VALID, "abc-100@yahoo.com", true },
+				{ Type.VALID, "abc111@abc.com", true }, { Type.VALID, "abc-100@abc.net", true },
+				{ Type.VALID, "abc.100@abc.com.au", true }, { Type.VALID, "abc@1.com", true },
+				{ Type.VALID, "abc@gmail.com.com", true }, { Type.VALID, "abc+100@gmail.com", true },
+
+				{ Type.INVALID, "abc", false }, { Type.INVALID, "abc@.com.my", false },
+				{ Type.INVALID, "abc123@gmail.a", false }, { Type.INVALID, "abc123@.com", false },
+				{ Type.INVALID, "abc123@.com.com", false }, { Type.INVALID, ".abc@abc.com", false },
+				{ Type.INVALID, "abc()*@gmail.com", false }, { Type.INVALID, "abc@%*.com", false },
+				{ Type.INVALID, "abc..2002@gmail.com", false }, { Type.INVALID, "abc.@gmail.com", false },
+				{ Type.INVALID, "abc@abc@gmail.com", false }, { Type.INVALID, "abc@gmail.com.1a", false },
+				{ Type.INVALID, "abc@gmail.com.aa.au", false } });
 	}
 
 	@Before
@@ -43,9 +55,16 @@ public class EmailTest {
 	}
 
 	@Test
-	public void testEmailValid_success() {
+	public void testEmailValid_success() throws InvalidInputException {
+		Assume.assumeTrue(type == Type.VALID);
 		boolean actual = userRegistration.EmailID(userEmail);
 		boolean expected = expectedResult;
 		assertEquals(expected, actual);
+	}
+
+	@Test(expected = InvalidInputException.class)
+	public void testEmailValid_fail() throws InvalidInputException {
+		Assume.assumeTrue(type == Type.INVALID);
+		userRegistration.EmailID(userEmail);
 	}
 }
---
+++ src/com/blz/juintTest/Exception/InvalidInputException.java
@@ -0,0 +1,10 @@
+package com.blz.juintTest.Exception;
+
+public class InvalidInputException extends Exception {
+	public InvalidInputException() {
+	}
+
+	public InvalidInputException(String msg) {
+		super(msg);
+	}
+}
