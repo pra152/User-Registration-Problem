61761fd30d99743e529578bac7d2451b3ea23e65
pra152 <pravatdas211@gmail.com>
2021-09-04 11:40:22 +0530

    Refactor: junit parametrized test to validate multiple entry of email

--- src/com/blz/juintTest/EmailTest.java
+++ src/com/blz/juintTest/EmailTest.java
@@ -1,21 +1,51 @@
 package com.blz.juintTest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import com.blz.userdetails.UserRegistration;
 
+@RunWith(Parameterized.class)
 public class EmailTest {
-	UserRegistration userRegistration = new UserRegistration();
+	private String userEmail;
+	private boolean expectedResult;
+	UserRegistration userRegistration;
+
+	public EmailTest(String userEmail, boolean expectedResult) {
+		this.userEmail = userEmail;
+		this.expectedResult = expectedResult;
+	}
+
+	@Parameterized.Parameters
+	public static Collection data() {
+		return Arrays.asList(new Object[][] { { "abc@yahoo.com", true }, { "abc-100@yahoo.com", true },
+				{ "abc-100@yahoo.com", true }, { "abc111@abc.com", true }, { "abc-100@abc.net", true },
+				{ "abc.100@abc.com.au", true }, { "abc@1.com", true }, { "abc@gmail.com.com", true },
+				{ "abc+100@gmail.com", true }, { "abc", false }, { "abc@.com.my", false }, { "abc123@gmail.a", false },
+				{ "abc123@.com", false }, { "abc123@.com.com", false }, { ".abc@abc.com", false },
+				{ "abc()*@gmail.com", false }, { "abc@%*.com", false }, { "abc..2002@gmail.com", false },
+				{ "abc.@gmail.com", false }, { "abc@abc@gmail.com", false }, { "abc@gmail.com.1a", false },
+				{ "abc@gmail.com.aa.au", false } });
+	}
+
+	@Before
+	public void initialize() {
+		userRegistration = new UserRegistration();
+	}
+
 	@Test
 	public void testEmailValid_success() {
-		assertTrue(userRegistration.EmailID("pravatdas211@gmail.com"));
-	}
-	
-	@Test()
-	public void testEmailValid_fail() {
-		assertFalse(userRegistration.EmailID("pavatdas@.com"));
+		boolean actual = userRegistration.EmailID(userEmail);
+		boolean expected = expectedResult;
+		assertEquals(expected, actual);
 	}
 }
